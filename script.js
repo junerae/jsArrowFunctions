5a57f729fe4669966b0199a7b46bdc6281d61617
June Djamaloedin <june.djamaloedin@gmail.com>
2021-07-23 17:51:34 +0200

    function arrows

---
+++ script.js
@@ -0,0 +1,19 @@
+// DEEL A
+
+// zet de functie om naar een arrow functie
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!")
+ikRockArrowFunctions()
+
+// DEEL B
+const fivePlusSeven = () => 5 + 7
+fivePlusSeven();
+
+// DEEL C
+let myFunction = (a, b) => a + b
+
+// DEEL D
+const addFive = (a) => a + 5
+
+// DEEL E
+const createObject = () => { greeting: "hoi" }
+
